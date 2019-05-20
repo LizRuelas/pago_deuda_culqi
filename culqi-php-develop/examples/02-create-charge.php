199e96807a258736c1c990b3785f738066cb50ef
LizRuelas <liz.ruelas.borda@gmail.com>
2019-05-20 15:48:15 -0500

    heroku

---
+++ culqi-php-develop/examples/02-create-charge.php
@@ -0,0 +1,33 @@
+<?php
+header('Content-Type: application/json');
+/**
+ * Como crear un cargo a una tarjeta usando Culqi PHP.
+ */
+
+// Cargar Culqi-PHP de forma manual
+  require '../Requests-master/library/Requests.php';
+  Requests::register_autoloader();
+  require '../lib/culqi.php';
+
+use Culqi\Culqi;
+// Configurar tu API Key y autenticación
+$SECRET_API_KEY = $_POST["secreta"];
+$culqi = new Culqi(array('api_key' => $SECRET_API_KEY));
+
+
+try {
+  // Creando Cargo a una tarjeta
+  $charge = $culqi->Charges->create(
+      array(
+        "amount" => $_POST["monto_back"],
+        "currency_code" =>$_POST["moneda"],
+        "email" => $_POST["email"],
+        "source_id" => $_POST["token"]
+        )
+  );
+  // Response
+  echo json_encode($charge);
+
+} catch (Exception $e) {
+  echo json_encode($e->getMessage());
+}
---
+++ js/app.js
@@ -0,0 +1,96 @@
+var moneda;
+
+$("#response-panel").hide();
+$("#response-panel-1").hide();
+$('#miBoton').on('click', function (e) {
+
+var monto = $("#monto").val();
+var monto_real = monto * 100;
+console.log(monto);
+console.log(monto_real)
+
+
+
+    // Abre el formulario con las opciones de Culqi.settings
+    if ($('#currency_code').is(":checked"))
+      {
+        console.log("soles");
+        moneda = "PEN";
+      } else {
+        console.log("dolares");
+        moneda = "USD";
+      }
+
+    Culqi.publicKey = $("#publica").val();
+
+    Culqi.settings({
+      title: 'Culqi Store',
+      currency: moneda,
+      description: 'Polo/remera Culqi lover',
+      amount: monto_real
+     });
+    Culqi.open();
+    e.preventDefault();
+});
+// Recibimos Token del Culqi.js
+  function culqi() {
+    var secreta = $("#secreta").val();
+    var monto_back = $("#monto").val() * 100;
+    if (Culqi.token) {
+        $(document).ajaxStart(function(){
+          run_waitMe();
+        });
+        // Imprimir Token
+        $.ajax({
+           type: 'POST',
+           url: '../culqi-php-develop/examples/02-create-charge.php',
+           data: { token: Culqi.token.id , moneda , secreta , email: Culqi.token.email , monto_back},
+           datatype: 'json',
+           success: function(data) {
+             var result = "";
+             if(data.constructor == String){
+                 result = JSON.parse(data);
+             }
+             if(data.constructor == Object){
+                 result = JSON.parse(JSON.stringify(data));
+             }
+             if(result.object === 'charge'){
+                resultdiv(result.outcome.merchant_message);
+                resultdiv1(result.outcome.user_message);
+             }
+             if(result.object === 'error'){
+                resultdiv(result.merchant_message);
+                resultdiv1(result.user_message);
+             }
+           },
+           error: function(error) {
+             resultdiv(error)
+           }
+        });
+    } else {
+      // Hubo un problema...
+      // Mostramos JSON de objeto error en consola
+      $('#response-panel').show();
+      $('#response').html(Culqi.error.merchant_message);
+      $('body').waitMe('hide');
+    }
+  };
+  function run_waitMe(){
+    $('body').waitMe({
+      effect: 'orbit',
+      text: 'VALIDACIÓN DE COMERCIO EN PRODUCCIÓN...',
+      bg: 'rgba(255,255,255,0.7)',
+      color:'#FF0000'
+    });
+  }
+  function resultdiv(message){
+    $('#response-panel').show();
+    $('#response').html(message);
+    $('body').waitMe('hide');
+  }
+
+  function resultdiv1(message1){
+    $('#response-panel-1').show();
+    $('#response-1').html(message1);
+    $('body').waitMe('hide');
+  }
